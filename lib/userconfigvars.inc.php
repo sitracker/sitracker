3e50de4c23a3da4e6ab5d318ce3b37ef9bb88034
Ivan Lucas <ivan@sitracker.org>
2010-03-06 12:34:52 +0000

    Make the "This incident will be logged against the customers CONTRACT" message option, based on user preference

--- lib/userconfigvars.inc.php
+++ lib/userconfigvars.inc.php
@@ -29,7 +29,9 @@ $CFGCAT['displayprefs'] = array('incident_refresh',
                                'incident_log_order',
                                'updates_per_page',
                                'show_table_legends',
-                               'incident_popup_onewindow'
+                               'incident_popup_onewindow',
+                               'show_confirmation_caution',
+                               'show_confirmation_delete'
                                );
 
 $CFGCAT['localeprefs'] = array('language','utc_offset');
@@ -74,12 +76,12 @@ $CFGVAR['theme']['title'] = $strInterfaceStyle;
 $CFGVAR['theme']['type'] = 'interfacestyleselect';
 
 // TODO
-$CFGVAR['show_confirmation_caution']['title'] = 'Show confirmation messages';
-$CFGVAR['show_confirmation_caution']['type'] = 'select';
+$CFGVAR['show_confirmation_caution']['title'] = 'Show general confirmation ("Are you sure?") messages';
+$CFGVAR['show_confirmation_caution']['type'] = 'checkbox';
 
 // TODO
-$CFGVAR['show_confirmation_delete']['title'] = 'Show confirmation messages on delete';
-$CFGVAR['show_confirmation_delete']['type'] = 'select';
+$CFGVAR['show_confirmation_delete']['title'] = 'Show confirmation messages ("Are you sure?") on delete';
+$CFGVAR['show_confirmation_delete']['type'] = 'checkbox';
 
 $CFGVAR['show_emoticons']['title'] = $strShowEmoticons;
 $CFGVAR['show_emoticons']['type'] = 'checkbox';
