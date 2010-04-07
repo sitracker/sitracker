41731d3ad19c60eb6317e1abbb2a1643e5c4569f
Paul Heaney <paul@sitracker.org>
2010-04-07 19:52:48 +0100

    * Removal of legacy comment and code formatting

--- edit_user_skills.php
+++ edit_user_skills.php
@@ -14,8 +14,8 @@
 
 if (empty($_REQUEST['user'])
     OR $_REQUEST['user'] == 'current'
-    OR $_REQUEST['userid']==$_SESSION['userid']) $permission=58; // Edit your software skills
-else $permission=59; // Manage users software skills
+    OR $_REQUEST['userid'] == $_SESSION['userid']) $permission = 58; // Edit your software skills
+else $permission = 59; // Manage users software skills
 
 require ('core.php');
 require (APPLICATION_LIBPATH . 'functions.inc.php');
@@ -32,13 +32,14 @@ $title = $strEditUserSkills;
 if (empty($submit))
 {
     include (APPLICATION_INCPATH . 'htmlheader.inc.php');
-    $sql = "SELECT * FROM `{$dbUserSoftware}` AS us, `{$dbSoftware}` AS s WHERE us.softwareid = s.id AND userid = '$user' ORDER BY name";
+    $sql = "SELECT * FROM `{$dbUserSoftware}` AS us, `{$dbSoftware}` AS s ";
+    $sql .= "WHERE us.softwareid = s.id AND userid = '{$user}' ORDER BY name";
     $result = mysql_query($sql);
     if (mysql_num_rows($result) >= 1)
     {
         while ($software = mysql_fetch_object($result))
         {
-            $expertise[]=$software->id;
+            $expertise[] = $software->id;
         }
     }
     echo "<h2>".icon('skill', 32)." ";
@@ -83,7 +84,8 @@ if (empty($submit))
     echo "<input type='button' value='&lt;&lt;' title='Remove All' onclick=\"copyAll(this.form.elements['expertise[]'],this.form.elements['noskills[]'])\" /><br />";
     echo "</td>";
     echo "<td class='shade1'>";
-    $sql = "SELECT * FROM `{$dbUserSoftware}` AS us, `{$dbSoftware}` AS s WHERE us.softwareid = s.id AND userid = '{$user}' ORDER BY name";
+    $sql = "SELECT * FROM `{$dbUserSoftware}` AS us, `{$dbSoftware}` AS s ";
+    $sql .= "WHERE us.softwareid = s.id AND userid = '{$user}' ORDER BY name";
     $result = mysql_query($sql);
     echo "<select name='expertise[]' multiple='multiple' size='20' style='width: 100%;  min-width: 200px;'>";
     while ($software = mysql_fetch_object($result))
@@ -106,23 +108,14 @@ if (empty($submit))
 else
 {
     // Update user profile
-    $selections=urldecode($_POST['choices']);
+    $selections = urldecode($_POST['choices']);
     parse_str($selections);
 
     $expertise = $_POST['expertise'];
     $noskills = $_POST['noskills'];
 
-    // remove existing selections first
-
-    // FIXME: This is going to wipe the backup/substitute engineer settings, need to fix this
-    // INL 21Dec04
-
     // FIXME: whatabout cases where the user is a backup for one of the products
     // he removes? or if the backup user leaves the company?
-
-    //$sql = "DELETE FROM `{$dbUserSoftware}` WHERE userid='{$_POST['userid']}'";
-    //mysql_query($sql);
-    //if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
     if (is_array($expertise))
     {
@@ -130,19 +123,19 @@ else
         foreach ($expertise AS $value)
         {
             $value = cleanvar($value);
-            $checksql = "SELECT userid FROM `{$dbUserSoftware}` WHERE userid='{$user}' AND softwareid='$value' LIMIT 1";
-            $checkresult=mysql_query($checksql);
+            $checksql = "SELECT userid FROM `{$dbUserSoftware}` WHERE userid='{$user}' AND softwareid='{$value}' LIMIT 1";
+            $checkresult = mysql_query($checksql);
             if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
             if (mysql_num_rows($checkresult)< 1)
             {
                 if ($value > 0)
                 {
-                    $sql = "INSERT DELAYED INTO `{$dbUserSoftware}` (userid, softwareid) VALUES ('{$user}', '$value')";
+                    $sql = "INSERT DELAYED INTO `{$dbUserSoftware}` (userid, softwareid) VALUES ('{$user}', '{$value}')";
                     mysql_query($sql);
                     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
                 }
             }
-            $softlist[]=$value;
+            $softlist[] = $value;
         }
     }
 
@@ -159,19 +152,19 @@ else
             if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
 
             // If we are providing backup for a skill we don't have - reset that back to nobody providing backup
-            $sql = "UPDATE `{$dbUserSoftware}` SET backupid='0' WHERE backupid='{$user}' AND softwareid='$value' LIMIT 1";
+            $sql = "UPDATE `{$dbUserSoftware}` SET backupid='0' WHERE backupid='{$user}' AND softwareid='{$value}' LIMIT 1";
             mysql_query($sql);
             if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_ERROR);
         }
     }
 
-    journal(CFG_LOGGING_MAX,'Skillset Updated',"Users Skillset was Changed",CFG_JOURNAL_USER,0);
+    journal(CFG_LOGGING_MAX, 'Skillset Updated', "Users Skillset was Changed", CFG_JOURNAL_USER, 0);
 
     // Have a look to see if any of the software we support is lacking a backup/substitute engineer
     $sql = "SELECT userid FROM `{$dbUserSoftware}` WHERE userid='{$user}' AND backupid='0' LIMIT 1";
-    $result=mysql_query($sql);
+    $result = mysql_query($sql);
     if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
-    $lacking=mysql_num_rows($result);
+    $lacking = mysql_num_rows($result);
     if ($lacking >= 1)
     {
         html_redirect("edit_backup_users.php?user={$user}", TRUE, $strYouShouldNowDefineSubstituteEngineers);
