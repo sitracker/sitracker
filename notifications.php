f4854b599570ca94ed8a74ef12906a7dd5dd08ec
Kieran Hogg <kieran@sitracker.org>
2010-04-15 00:27:18 +0100

    * half attempt at a trigger check GUI, just to get code commited

--- notifications.php
+++ notifications.php
@@ -1,5 +1,5 @@
 <?php
-// triggers.php - Page for setting user trigger preferences
+// notifications.php - Page for users to setup their trigger actions
 //
 // SiT (Support Incident Tracker) - Support call tracking system
 // Copyright (C) 2000-2009 Salford Software Ltd. and Contributors
@@ -15,20 +15,23 @@ require (APPLICATION_LIBPATH . 'trigger.class.php');
 // This page requires authentication
 require (APPLICATION_LIBPATH . 'auth.inc.php');
 
-$adminuser = user_permission($sit[2],22); // Admin user
-$title = 'New Triggers Interface';
+$title = $strNotifications;
 include (APPLICATION_INCPATH . 'htmlheader.inc.php');
-($_GET['user'] == 'admin') ? $user = 0 : $user = $sit[2];
 
 echo "<h2>".icon('trigger', 32)." {$title}</h2>";
+echo "<div id='pageintro'>";
+echo "<p>Below is a list of your existing notifications.</p>";
+echo "<a href='action_details.php'>";
+echo icon('add', 16). " {$strAddNotification}</a>";
+echo "</div><br />";
 // BEGIN TESTING CODE
-$t = new TriggerEvent('TRIGGER_SIT_UPGRADED', array());
+//$t = new TriggerEvent('TRIGGER_SIT_UPGRADED', array());
 
 //$t = new Trigger('TRIGGER_INCIDENT_CLOSED', 1, 'EMAIL_INCIDENT_CLOSURE', 'ACTION_EMAIL', '', '');
 //echo $t->debug();
 // $t2 = Trigger::byID(5);
 // $t2->debug();
-echo triggers_to_html($user);
+echo triggers_to_html($sit[2]);
 
 //END TESTING CODE
 include (APPLICATION_INCPATH . 'htmlfooter.inc.php');
