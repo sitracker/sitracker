5d494c0f5a59f4cb1b2ccd4e1d4a3e4196313564
Paul Heaney <paul@sitracker.org>
2011-03-19 13:22:20 +0000

    format

--- lib/html_drop_downs.inc.php
+++ lib/html_drop_downs.inc.php
@@ -128,7 +128,7 @@ function userstatus_drop_down($name, $id = 0, $userdisable = FALSE)
     // extract statuses
     $sql  = "SELECT id, name FROM `{$dbUserStatus}` ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $html = "<select name='$name'>\n";
     if ($userdisable)
@@ -169,7 +169,7 @@ function userstatus_bardrop_down($name, $id)
     // extract statuses
     $sql  = "SELECT id, name FROM `{$dbUserStatus}` ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $html = "<select id='userstatus_dropdown' name='$name' title='{$GLOBALS['strSetYourStatus']}' ";
     $html .= "onchange=\"set_user_status();\" onblur=\"hide_status_drop_down();\">";
@@ -212,7 +212,7 @@ function emailtemplate_drop_down($name, $id, $type)
 
     $sql  = "SELECT id, name, description FROM `{$dbEmailTemplates}` WHERE type='{$type}' ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $html = "<select name=\"{$name}\">";
     if ($id == 0)
@@ -280,7 +280,7 @@ function escalation_path_drop_down($name, $id)
     $sql  = "SELECT id, name FROM `{$dbEscalationPaths}` ";
     $sql .= "ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
     $html = "<select name='{$name}' id='{$name}' >";
     $html .= "<option selected='selected' value='0'>{$GLOBALS['strNone']}</option>\n";
     while ($path = mysql_fetch_object($result))
@@ -343,7 +343,7 @@ function group_drop_down($name = '', $selected = '')
     {
         foreach ($grouparr AS $groupid => $groupname)
         {
-            $html .= "<option value='$groupid'";
+            $html .= "<option value='{$groupid}'";
             if ($groupid == $selected)
             {
                 $html .= " selected='selected'";
@@ -371,7 +371,7 @@ function product_drop_down($name, $id, $required = FALSE)
     // extract products
     $sql  = "SELECT id, name FROM `{$dbProducts}` ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $html = "<select name='{$name}' id='{$name}'";
     if ($required)
@@ -416,7 +416,7 @@ function skill_drop_down($name, $id)
     $sql  = "SELECT id, name, lifetime_end FROM `{$dbSoftware}` ";
     $sql .= "ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $html = "<select name='{$name}' id='{$name}' >";
 
@@ -464,7 +464,7 @@ function softwareproduct_drop_down($name, $id, $productid, $visibility='internal
     $sql .= "AND productid = '$productid' ";
     $sql .= "ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
 
     $numrows = mysql_num_rows($result);
     if ($numrows > 0)
@@ -522,7 +522,7 @@ function vendor_drop_down($name, $id, $required = FALSE)
     global $dbVendors;
     $sql = "SELECT id, name FROM `{$dbVendors}` ORDER BY name ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
     $html = "<select name='$name'";
     if ($required)
     {
@@ -562,7 +562,7 @@ function sitetype_drop_down($name, $id)
     global $dbSiteTypes;
     $sql = "SELECT typeid, typename FROM `{$dbSiteTypes}` ORDER BY typename ASC";
     $result = mysql_query($sql);
-    if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
+    if (mysql_error()) trigger_error(mysql_error(), E_USER_WARNING);
     $html .= "<select name='$name'>\n";
     if ($id == 0)
     {
@@ -890,7 +890,7 @@ function software_backup_dropdown($name, $userid, $softwareid, $backupid)
     }
     else
     {
-        $html .= "<input type='hidden' name='$name' value='0' />{$GLOBALS['strNoneAvailable']}";
+        $html .= "<input type='hidden' name='{$name}' value='0' />{$GLOBALS['strNoneAvailable']}";
     }
     return ($html);
 }
