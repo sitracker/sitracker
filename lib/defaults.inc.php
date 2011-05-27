ab10a98879988f55b29549759b7d6d708a98d657
Paul Heaney <paul@sitracker.org>
2011-05-27 20:02:23 +0100

    add default for portal iconset

--- lib/defaults.inc.php
+++ lib/defaults.inc.php
@@ -236,6 +236,7 @@ $CONFIG['feedback_enabled'] = TRUE;
 
 $CONFIG['portal_site_incidents'] = TRUE; //users in the portal can view site incidents based on the contract options
 $CONFIG['portal_usernames_can_be_changed'] = TRUE; //portal usernames can be changed by the users
+$CONFIG['portal_iconset'] = 'kriplyana';
 
 // The interface style to use for the portal
 $CONFIG['portal_interface_style'] = 8;
