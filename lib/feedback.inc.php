4c0ae5030ea9cdc9ebb669af7f9c6cc9d1cc4798
Carsten Jensen <tomse@tomse.dk>
2011-06-04 20:59:22 +0200

    moved functions into functions file

--- lib/feedback.inc.php
+++ lib/feedback.inc.php
@@ -85,4 +85,200 @@ function feedback_hash($formid, $contactid, $incidentid)
     return $hashcode;
 }
 
+/**
+ * @author Ivan Lucas
+ * @param string $name. Field name
+ * @param string $required. 'true' or 'false' is the field mandatory?
+ * @param string $options. delimited list of options
+ * @param string $answer (optional).
+ * @returns string HTML
+ */
+function feedback_html_rating($name, $required, $options, $answer='')
+{
+    global $CONFIG;
+    // Rate things out of 'score_max' number
+    $score_max = $CONFIG['feedback_max_score'];
+
+    $option_list = explode('{@}', $options);
+    $promptleft = $option_list[0];
+    $promptright = $option_list[1];
+
+    $colwidth = round(100/$score_max);
+
+    $html = "<table class='feedback'>\n";
+    if (empty($promptleft) == FALSE OR empty($promptright) == FALSE)
+    {
+        $html .= "<tr>";
+        $html .= "<th colspan='{$score_max}' style='text-align: left;'>";
+        $html .= "<div style='float: right;'>{$promptright}</div><div>{$promptleft}</div></th>";
+        if ($required != 'true')
+        {
+            $html .= "<th>&nbsp;</th>";
+        }
+        $html .= "</tr>\n";
+    }
+
+    $html .= "<tr>\n";
+    for ($c = 1; $c <= $score_max; $c++)
+    {
+        $html .= "<td width='{$colwidth}%' style='text-align: center;'><input type='radio' name='{$name}' value='{$c}' ";
+        if ($answer == $c)
+        {
+            $html .= "checked='checked'";
+        }
+        $html .= " />$c</td>\n";
+    }
+
+    if ($required != 'true')
+    {
+        $html .= "<td><input type='radio' name='{$name}' value='0' ";
+        if ($answer == 0)
+        {
+            $html .= "checked='checked'";
+        }
+        $html .= "/>{$strNotApplicableAbbrev}</td>";
+    }
+    $html .= "</tr>\n";
+    $html .= "</table>\n";
+
+    return $html;
+}
+
+
+/**
+ * @author Ivan Lucas
+ * @param string $name. Field name
+ * @param string $required. 'true' or 'false' is the field mandatory?
+ * @param string $options. delimited list of options
+ * @param string $answer (optional).
+ */
+function feedback_html_options($name, $required, $options, $answer='')
+{
+    $option_list = explode('{@}', $options);
+    $option_count = count($option_list);
+    if ($option_count > 3)
+    {
+        $html .= "<select name='{$name}'>\n";
+        foreach ($option_list AS $key=>$option)
+        {
+            $value = strtolower(trim(str_replace(' ', '_', $option)));
+            $html .= "<option value='{$value}'";
+            if ($answer == $value)
+            {
+                $html .= " selected='selected'";
+            }
+            $html .= ">".trim($option)."</option>\n";
+        }
+        $html .= "</select>\n";
+    }
+    else
+    {
+        foreach ($option_list AS $key=>$option)
+        {
+            $value = strtolower(trim(str_replace(' ', '_', $option)));
+            $html .= "<input type='radio' name='{$name}' value='{$value}'";
+            if ($answer == $value)
+            {
+                $html .= " selected='selected'";
+            }
+            $html .= " />".trim($option)." &nbsp; \n";
+        }
+    }
+    return $html;
+}
+
+
+/**
+ * @author Ivan Lucas
+ * @param string $name. Field name
+ * @param string $required. 'true' or 'false' is the field mandatory?
+ * @param string $options. delimited list of options
+ */
+function feedback_html_multioptions($name, $required, $options)
+{
+    $option_list = explode('{@}', $options);
+    $option_count = count($option_list);
+    if ($option_count > 3)
+    {
+        $html .= "<select name='{$name}[]' multiple='multiple'>\n";
+        foreach ($option_list AS $key=>$option)
+        {
+            $value = strtolower(trim(str_replace(' ', '_', $option)));
+            $html .= "<option value='{$value}'>".trim($option)."</option>\n";
+        }
+        $html .= "</select>\n";
+    }
+    else
+    {
+        foreach ($option_list AS $key=>$option)
+        {
+            $value = strtolower(trim(str_replace(' ', '_', $option)));
+            $html .= "<input type='checkbox' name='{$name}' value='{$value}' />".trim($option)." &nbsp; \n";
+        }
+    }
+    return $html;
+}
+
+
+/**
+ * @author Ivan Lucas
+ * @param string $name. Field name
+ * @param string $required. 'true' or 'false' is the field mandatory?
+ * @param string $options. delimited list of options
+ * @param string $answer (optional).
+ */
+function feedback_html_text($name, $required, $options, $answer='')
+{
+    $option_list = explode('{@}', $options);
+    $cols = $option_list[0] ? $option_list[0] : 60;
+    $rows = $option_list[1] ? $option_list[1] : 5;
+
+    if ($rows == 1)
+    {
+        $html .= "<input type='text' name='{$name}' size='{$cols}' value='{$answer}' />\n";
+    }
+    else
+    {
+        $html .= "<textarea name ='{$name}' rows='{$rows}' cols='{$cols}' >{$answer}</textarea>\n";
+    }
+
+    return $html;
+}
+
+
+/**
+ * @author Ivan Lucas
+ * @param string $name. Field name
+ * @param string $required. 'true' or 'false' is the field mandatory?
+ * @param string $options. delimited list of options
+ * @param string $answer (optional).
+ */
+function feedback_html_question($type, $name, $required, $options, $answer='')
+{
+    $options = nl2br(trim($options));
+    $options = str_replace('<br>', '{@}', $options);
+    $options = str_replace('<br />', '{@}', $options);
+    $options = str_replace('<br/>', '{@}', $options);
+    switch ($type)
+    {
+        case 'rating':
+            $html = feedback_html_rating($name, $required, $options, $answer);
+            break;
+        case 'options':
+            $html = feedback_html_options($name, $required, $options, $answer);
+            break;
+        case 'multioptions':
+            $html = feedback_html_multioptions($name, $required, $options, $answer);
+            break;
+        case 'text':
+            $html = feedback_html_text($name, $required, $options, $answer);
+            break;
+        default:
+            $html = sprintf($GLOBALS['strErrorNoHandlerDefinedForQuestionTypeX'], $type);
+            break;
+  }
+  return $html;
+}
+
+
 ?>
