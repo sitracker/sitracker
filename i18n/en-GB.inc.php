ab7b1ead5e58faa6bc20eff5789bb54350c93b82
Carsten Jensen <tomse@tomse.dk>
2011-06-05 19:15:07 +0200

    Fixed a previously introduced bug, we apparently use the Qx for naming in forms, strings removed. multioptions were fixed as mb_string doesn't handle arrays, and we now save the array as a comma separated file

--- i18n/en-GB.inc.php
+++ i18n/en-GB.inc.php
@@ -1427,8 +1427,6 @@ $strPublish = 'Publish';
 $strPublished = 'Published';
 $strPublishedToday = 'Published Today';
 $strPublishedOnX = 'Published on %s';
-# Abbrev for question
-$strQ = 'Q';
 $strQualifications = 'Qualifications';
 $strQueryByExample = 'Query By Example';
 $strQuestion = 'Question';
