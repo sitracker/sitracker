efe206106e8d0490e867df34e7802f9a3ae0bd1f
Ivan Lucas <ivan@sitracker.org>
2011-06-06 17:50:16 +0100

    cs-CZ (Czech) language file 2.96%

---
+++ i18n/cs-CZ.inc.php
@@ -0,0 +1,69 @@
+<?php
+// SiT! Language File - Český (cs-CZ)
+
+$languagestring = 'Český (cs-CZ)';
+$i18ncharset = 'UTF-8';
+
+// List of letters of the alphabet for this language
+// in standard alphabetical order (upper case, where applicable). Leave blank to disable alphabetical indexing.
+$i18nAlphabet = 'AÁBCČDĎEÉĚFGHIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽ';
+
+// list of strings (Alphabetical by key)
+$strAll = 'Všichni';
+$strAmount = 'Částka';
+$strAssignTo = 'Přiřadit k';
+$strBalance = 'Bilance';
+$strBilling = 'Fakturace';
+$strBytes = 'Bajtů';
+$strCategory = 'Kategorie';
+$strClosed = 'Uzavřeno';
+$strDays = 'Dny';
+$strDelete = 'Zrušit';
+$strDescription = 'Popis';
+$strEdit = 'Upravit';
+$strEmail = 'E-mail';
+$strEnabled = 'Povolený';
+$strEndDate = 'Koncové datum';
+$strField = 'Pole';
+$strFilename = 'Název souboru';
+$strFrom = 'Od';
+$strID = 'ID';
+$strInstall = 'Instalovat';
+$strKey = 'Klíč';
+$strLanguage = 'Jazyk';
+$strLogIn = 'Přihlášení';
+$strLogin = 'Přihlášení';
+$strLogout = 'Odhlásit';
+$strManageUsers = 'Správa uživatelů';
+$strMessage = 'Zpráva';
+$strNew = 'Nové';
+$strNo = 'Ne';
+$strOK = 'OK';
+$strPassword = 'Heslo';
+$strPriority = 'Priorita';
+$strPrivate = 'Soukromý';
+$strPublic = 'Veřejný';
+$strPublish = 'Publikovat';
+$strRealName = 'Skutečné jméno';
+$strReopen = 'Znovuotevřít';
+$strResetPassword = 'Změnit heslo';
+$strShowAll = 'Zobrazit vše';
+$strSortAscending = 'Vzestupně';
+$strSortDescending = 'Sestupně';
+$strStartDate = 'Počáteční datum';
+$strStatus = 'Stav';
+$strSummary = 'Shrnutí';
+$strTag = 'Značky';
+$strTo = 'Komu';
+$strTotal = 'Celkem';
+$strType = 'Typ';
+$strUpgrade = 'Upgradovat';
+$strUpdated = 'Aktualizace';
+$strUnassigned = 'Nepřiřazeno';
+$strUsers = 'Uživatelé';
+$strUsername = 'Uživatelské jméno';
+$strValue = 'Hodnota';
+$strVersion = 'Verze';
+$strView = 'Zobrazit';
+$strYes = 'Ano';
+?>
