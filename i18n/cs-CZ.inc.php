5b3df61b6cba230d4d6cc787e74032c67de4c4f4
Ivan Lucas <ivan@sitracker.org>
2011-06-06 18:06:46 +0100

    Whitespacing

--- i18n/cs-CZ.inc.php
+++ i18n/cs-CZ.inc.php
@@ -12,58 +12,76 @@ $i18nAlphabet = 'AÁBCČDĎEÉĚFGHIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽ';
 $strAll = 'Všichni';
 $strAmount = 'Částka';
 $strAssignTo = 'Přiřadit k';
+
 $strBalance = 'Bilance';
 $strBilling = 'Fakturace';
 $strBytes = 'Bajtů';
+
 $strCategory = 'Kategorie';
 $strClosed = 'Uzavřeno';
+
 $strDays = 'Dny';
 $strDelete = 'Zrušit';
 $strDescription = 'Popis';
+
 $strEdit = 'Upravit';
 $strEmail = 'E-mail';
 $strEnabled = 'Povolený';
 $strEndDate = 'Koncové datum';
+
 $strField = 'Pole';
 $strFilename = 'Název souboru';
 $strFrom = 'Od';
+
 $strID = 'ID';
 $strInstall = 'Instalovat';
+
 $strKey = 'Klíč';
+
 $strLanguage = 'Jazyk';
 $strLogIn = 'Přihlášení';
 $strLogin = 'Přihlášení';
 $strLogout = 'Odhlásit';
+
 $strManageUsers = 'Správa uživatelů';
 $strMessage = 'Zpráva';
+
 $strNew = 'Nové';
 $strNo = 'Ne';
+
 $strOK = 'OK';
+
 $strPassword = 'Heslo';
 $strPriority = 'Priorita';
 $strPrivate = 'Soukromý';
 $strPublic = 'Veřejný';
 $strPublish = 'Publikovat';
+
 $strRealName = 'Skutečné jméno';
 $strReopen = 'Znovuotevřít';
 $strResetPassword = 'Změnit heslo';
+
 $strShowAll = 'Zobrazit vše';
 $strSortAscending = 'Vzestupně';
 $strSortDescending = 'Sestupně';
 $strStartDate = 'Počáteční datum';
 $strStatus = 'Stav';
 $strSummary = 'Shrnutí';
+
 $strTag = 'Značky';
 $strTo = 'Komu';
 $strTotal = 'Celkem';
 $strType = 'Typ';
+
 $strUpgrade = 'Upgradovat';
 $strUpdated = 'Aktualizace';
 $strUnassigned = 'Nepřiřazeno';
 $strUsers = 'Uživatelé';
 $strUsername = 'Uživatelské jméno';
+
 $strValue = 'Hodnota';
 $strVersion = 'Verze';
 $strView = 'Zobrazit';
+
 $strYes = 'Ano';
 ?>
