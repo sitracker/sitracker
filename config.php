sitracker/sitracker

commit d00e277a78a8eb035bcf93eaa5a2b52d445cc240

only tidy variables once (Mantis 1517)

config.php
@@ -60,7 +60,7 @@ if ($action == 'save' AND ($CONFIG['demo'] !== TRUE OR $_SESSION['userid'] == 1)
         $savevar = array();
         foreach ($CFGCAT[$selcat] AS $catvar)
         {
-            $value = cleanvar($_REQUEST[$catvar]);
+            $value = $_REQUEST[$catvar]; // NOTE: we don't clean here we do this after we've manipulated the value but before insert
             // Type conversions
             switch ($CFGVAR[$catvar]['type'])
             {
@@ -130,7 +130,7 @@ if ($action == 'save' AND ($CONFIG['demo'] !== TRUE OR $_SESSION['userid'] == 1)
                     $value = intval($value);
                     break;
             }
-            $savevar[$catvar] = mysql_real_escape_string($value);
+            // $savevar[$catvar] = mysql_real_escape_string($value);
             if (mb_substr($value, 0, 6) == 'array(')
             {
                 eval("\$val = $value;");

commit e4e959cec308128841ac2f2444fdc469627d44b0

Fix saving of 2darray config variables which was broken by commit: https://gitorious.org/sit/sit/commit/d00e277a78a8eb035bcf93eaa5a2b52d445cc240

config.php
@@ -88,6 +88,7 @@ if ($action == 'save' AND ($CONFIG['demo'] !== TRUE OR $_SESSION['userid'] == 1)
                     break;
 
                 case '2darray':
+                    $value = cleanvar($value);
                     $value = str_replace('\n', ',', $value);
                     $value = str_replace('\r', '', $value);
                     $value = str_replace("\r", '', $value);