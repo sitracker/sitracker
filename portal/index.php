5e141873fc5bfbaf08f6a989ace329c3567bd937
Ivan Lucas <ivan@sitracker.org>
2011-06-18 13:06:49 +0100

    filter $showclosed variable better.

--- portal/index.php
+++ portal/index.php
@@ -19,7 +19,7 @@ include (APPLICATION_LIBPATH . 'portalauth.inc.php');
 include (APPLICATION_INCPATH . 'portalheader.inc.php');
 
 // External variables
-$showclosed = cleanvar($_REQUEST['showclosed']);
+$showclosed = clean_fixed_list($_REQUEST['showclosed'], array('true', 'false'));
 $site = clean_int($_REQUEST['site']);
 $sort = cleanvar($_REQUEST['sort']);
 $order = clean_fixed_list($_REQUEST['order'], array('a','d'));
