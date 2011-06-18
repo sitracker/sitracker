bfaac40d2fa948c93e72a1b66a435cadcf415f41
Ivan Lucas <ivan@sitracker.org>
2011-06-18 17:19:34 +0100

    Show some more information on the portal feedback forms list, also make the table look more like other SiT! tables.

--- portal/feedback.php
+++ portal/feedback.php
@@ -20,22 +20,36 @@ if (($CONFIG['portal_feedback_enabled'] == FALSE) OR ($CONFIG['feedback_enabled'
     header("Location: index.php");
 }
 
-$sql = "SELECT formid, incidentid FROM `{$dbFeedbackRespondents}` ";
+$sql = "SELECT formid, incidentid, created, email FROM `{$dbFeedbackRespondents}` ";
 $sql .= "WHERE contactid = '{$_SESSION['contactid']}' ";
 $sql .= "AND completed = 'no'";
 $result = mysql_query($sql);
 if (mysql_error()) trigger_error(mysql_error(),E_USER_WARNING);
 if (mysql_num_rows($result) < 1)
 {
-    $html = "<p align='center'>{$strNoFeedbackFormsAvailable}</p>";
+    $html = user_alert($strNoFeedbackFormsAvailable, E_USER_INFO);
 }
 else
 {
+    $shade = 'shade1';
     $html = "<table align='center'>";
+    $html .= "<tr>";
+    $html .= colheader('created', $strDate);
+    $html .= colheader('incident', $strIncident);
+    $html .= colheader('email', $strEmail);
+    $html .= colheader('action', $strAction);
+
+    $html .= "</tr>";
     while ($row = mysql_fetch_object($result))
     {
         $hashcode = feedback_hash($row->formid, $_SESSION['contactid'], $row->incidentid);
-        $html .= "<tr><td><a target='_blank' href='" . application_url() . "feedback.php?ax={$hashcode}'>{$strFeedbackFormForIncidentX} : {$row->incidentid}</a></td></tr>";
+        $html .= "<tr class='{$shade}'>";
+        $html .= "<td>".ldate($CONFIG['dateformat_datetime'], mysql2date($row->created))."</td>";
+        $html .= "<td>[{$row->incidentid}] - ".incident_title($row->incidentid)."</td>";
+        $html .= "<td>{$row->email}</td>";
+        $html .= "<td><a target='_blank' href='" . application_url() . "feedback.php?ax={$hashcode}'>{$strView}</a></td></tr>";
+        if ($shade == 'shade1') $shade = 'shade2';
+        else $shade = 'shade1';
     }
     $html .= "</table>";
 }
