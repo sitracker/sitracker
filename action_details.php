e10613a1e4ab9e2385600823c605f103d0d21332
Carsten Jensen <tomse@tomse.dk>
2011-07-02 13:35:10 +0200

    started moving styling from code to css

--- action_details.php
+++ action_details.php
@@ -101,7 +101,7 @@ switch ($action)
 
     default:
         echo "<h2>{$strNewAction}</h2>";
-        echo "<div id='container'>";
+        echo "<div id='actiondetailscontainer'>";
         echo "<form id='newtrigger' method='post' action='{$_SERVER['PHP_SELF']}'>";
         if ($trigger_mode == 'system')
         {
@@ -109,7 +109,7 @@ switch ($action)
             echo "<p>{$strWhichAction}</p>";
         }
         echo "<h3>{$strAction}</h3>";
-        echo "<p style='text-align:left'>{$strChooseWhichActionNotify}</p>";
+        echo "<p>{$strChooseWhichActionNotify}</p>";
         echo "<select id='triggertype' name='triggertype' onchange='switch_template()' onkeyup='switch_template()'>";
         foreach ($trigger_types as $name => $trigger)
         {
@@ -122,7 +122,7 @@ switch ($action)
         echo "</select>";
 
         echo "<h3>{$strNotificationMethod}</h3>";
-        echo "<p style='text-align:left'>$strChooseWhichMethodNotification</p>";
+        echo "<p>$strChooseWhichMethodNotification</p>";
         echo "<select id='new_action' name='new_action' onchange='switch_template()' onkeyup='switch_template()'>";
         echo "<option/>";
         foreach ($actionarray as $name => $action)
@@ -135,24 +135,24 @@ switch ($action)
         }
         echo "</select>";
 
-        echo "<div id='emailtemplatesbox' style='display:none'>";
+        echo "<div id='emailtemplatesbox'>";
         echo "<h3>{$strEmailTemplate}</h3> ";
 
-        echo "<p style='text-align:left'>$strChooseWhichTemplate</p>";
+        echo "<p>$strChooseWhichTemplate</p>";
         echo email_templates('emailtemplate', $trigger_mode)."</div>";
 
-        echo "<div id='noticetemplatesbox' style='display:none'>";
+        echo "<div id='noticetemplatesbox'>";
 
         echo "<h3>{$strNoticeTemplate}</h3> ";
-        echo "<p style='text-align:left'>{$strChooseWhichTemplate}</p>";
+        echo "<p>{$strChooseWhichTemplate}</p>";
         echo notice_templates('noticetemplate')."</div>";
-        echo '<div id="checksbox" style="display:none">';
+        echo "<div id='checksbox'>";
 
         echo "<h3>{$strConditions}</h3>";
-        echo "<p style='text-align:left'>{$strSomeActionsOptionalConditions}</p>";
-        echo "<p style='text-align:left'>{$strExampleWhenIncidentAssigned} ";
+        echo "<p>{$strSomeActionsOptionalConditions}</p>";
+        echo "<p>{$strExampleWhenIncidentAssigned} ";
         echo "{$strAddingACondition}</p>" ;
-        echo "<div id='checkshtml'></div></div>";
+        echo "</div>";
         echo "<input type='hidden' name='action' value='save' />";
         echo "<br /><p class='formbuttons'><input type='reset' name='reset' value='{$strReset}' /> ";
         echo "<input type='submit' name='submit' value='{$strSave}' /></p>";
@@ -166,7 +166,7 @@ switch ($action)
     //             echo 'Only notify when '. $data['description']. ' is ' .$data['checkreplace'](),"<br />";
     //         }
     //     }
-        echo "<p align='center'><a href='notifications.php'>{$strReturnWithoutSaving}</a></p>";
+        echo "<p class='notification'><a href='notifications.php'>{$strReturnWithoutSaving}</a></p>";
         include (APPLICATION_INCPATH . 'htmlfooter.inc.php');
 }
 
