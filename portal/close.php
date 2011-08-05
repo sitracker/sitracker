0c6e155f142b5c96b8951f9399fc11fd2e93838f
Paul Heaney <paul@sitracker.org>
2011-08-05 19:56:07 +0100

    fix issue were request closure wasn't saved when name had a apostrophie

--- portal/close.php
+++ portal/close.php
@@ -52,7 +52,7 @@ if ($incidentcontact == $_SESSION['contactid'])
         if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
 
         // FIXME i18n ? In db ? - CJ added syslang here - syslang isn't saved for some reason bug 1618
-        $reason = "{$SYSLANG['strRequestClosureViaThePortalBy']} [b]{$user->forenames} {$user->surname}[/b]\n\n";
+        $reason = cleanvar("{$SYSLANG['strRequestClosureViaThePortalBy']} [b]{$user->forenames} {$user->surname}[/b]\n\n");
         $reason .= "<b>{$SYSLANG['strReason']}:</b> ".cleanvar($_REQUEST['reason']);
         $owner = incident_owner($id);
         $sql = "INSERT into `{$dbUpdates}` (incidentid, userid, type, currentowner, currentstatus, bodytext, timestamp, customervisibility) ";
