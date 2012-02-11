3a1b5003f35f22ff6a1de350fa49f8da7ea9dcba
Paul Heaney <paul@sitracker.org>
2012-02-11 16:56:46 +0000

    remove escalate link, committed by mistake

--- inc/incident_html_top.inc.php
+++ inc/incident_html_top.inc.php
@@ -247,7 +247,6 @@ if ($menu != 'hide')
         echo "<a class='barlink' href='{$CONFIG['application_webpath']}incident_attachments.php?id={$id}&amp;popup={$popup}' accesskey='F'>{$strFiles}</a> | ";
         if ($servicelevel->timed == 'yes') echo "<a class='barlink' href='{$CONFIG['application_webpath']}tasks.php?incident={$id}'>{$strActivities}</a> | ";
         echo "<a class='barlink' href='{$CONFIG['application_webpath']}incident_details.php?id={$id}&amp;popup={$popup}' accesskey='D'>{$strDetailsAndLog}</a> | ";
-        echo "<a class='barlink' href='{$CONFIG['application_webpath']}escalate.php?id={$id}&amp;popup={$popup}' accesskey='D'>ESCALATE</a> | ";
         plugin_do('incident_html_top_menu_open_incident');
         echo "<a class='barlink' target='top.opener' href='{$CONFIG['application_webpath']}help.php'>{$strHelpChar}</a>";
     }
