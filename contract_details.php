b9b498f4a0fd8a3584dde28f70a55c9ff4f081b6
Paul Heaney <paul@sitracker.org>
2012-12-16 16:21:59 +0000

    removal of bad html function contract_details

--- contract_details.php
+++ contract_details.php
@@ -29,7 +29,245 @@ include (APPLICATION_INCPATH . 'htmlheader.inc.php');
 plugin_do('contract_details');
 
 // Display Maintenance
-echo contract_details($id);
+$sql  = "SELECT m.*, m.notes AS maintnotes, s.name AS sitename, ";
+$sql .= "r.name AS resellername, lt.name AS licensetypename ";
+$sql .= "FROM `{$dbMaintenance}` AS m, `{$dbSites}` AS s, ";
+$sql .= "`{$dbResellers}` AS r, `{$dbLicenceTypes}` AS lt ";
+$sql .= "WHERE s.id = m.site ";
+$sql .= "AND m.id='{$id}' ";
+$sql .= "AND m.reseller = r.id ";
+$sql .= "AND (m.licence_type IS NULL OR m.licence_type = lt.id) ";
+if ($mode == 'external') $sql .= "AND m.site = '{$_SESSION['siteid']}'";
+
+$maintresult = mysql_query($sql);
+if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+
+$maint = mysql_fetch_object($maintresult);
+
+$html = "<table class='maintable vertical'>";
+echo "<tr><th>{$strContract} {$strID}:</th>";
+echo "<td><h3>".icon('contract', 32)." ";
+echo "{$maint->id}</h3></td></tr>";
+echo "<tr><th>{$strStatus}:</th><td>";
+if ($maint->term == 'yes')
+{
+    echo "<strong>{$strTerminated}</strong>";
+}
+else
+{
+    echo $strActive;
+}
+
+if ($maint->expirydate < $now AND $maint->expirydate != '-1')
+{
+    echo ", <span class='expired'>{$strExpired}</span>";
+}
+echo "</td></tr>\n";
+echo "<tr><th>{$strSite}:</th>";
+
+echo "<td><a href=\"site_details.php?id=".$maint->site."\">".$maint->sitename."</a></td></tr>";
+
+echo "<tr><th>{$strAdminContact}:</th>";
+
+echo "<td><a href=\"contact_details.php?id=";
+echo "{$maint->admincontact}\">";
+echo contact_realname($maint->admincontact)."</a></td></tr>";
+
+echo "<tr><th>{$strReseller}:</th><td>";
+
+if (empty($maint->resellername))
+{
+    echo $strNoReseller;
+}
+else
+{
+    echo $maint->resellername;
+}
+echo "</td></tr>";
+echo "<tr><th>{$strProduct}:</th><td>".product_name($maint->product)."</td></tr>";
+echo "<tr><th>{$strIncidents}:</th>";
+echo "<td>";
+$incidents_remaining = $maint->incident_quantity - $maint->incidents_used;
+
+if ($maint->incident_quantity == 0)
+{
+    $quantity = $strUnlimited;
+}
+else
+{
+    $quantity = $maint->incident_quantity;
+}
+
+echo sprintf($strUsedNofN, $maint->incidents_used, $quantity);
+if ($maint->incidents_used >= $maint->incident_quantity AND
+    $maint->incident_quantity != 0)
+{
+    echo " ({$strZeroRemaining})";
+}
+
+echo "</td></tr>";
+if ($maint->licence_quantity != '0')
+{
+    echo "<tr><th>{$strLicense}:</th>";
+    echo "<td>{$maint->licence_quantity} {$maint->licensetypename}</td></tr>\n";
+}
+
+echo "<tr><th>{$strServiceLevel}:</th><td>".get_sla_name($maint->servicelevel)."</td></tr>";
+echo "<tr><th>{$strExpiryDate}:</th><td>";
+if ($maint->expirydate == '-1')
+{
+    echo "{$strUnlimited}";
+}
+else
+{
+    echo ldate($CONFIG['dateformat_date'], $maint->expirydate);
+}
+
+echo "</td></tr>";
+
+$timed = servicelevel_timed($maint->servicelevel);
+echo "<tr><th>{$strService}</th><td>";
+echo contract_service_table($id, $timed);
+echo "</td></tr>\n";
+
+if ($timed)
+{
+    echo "<tr><th>{$strBalance}</th><td>{$CONFIG['currency_symbol']}".number_format(get_contract_balance($id, TRUE, TRUE), 2);
+    $multiplier = get_billable_multiplier(strtolower(date('D', $now)), date('G', $now));
+    echo " (&cong;".contract_unit_balance($id, TRUE, TRUE)." units)";
+    echo "</td></tr>";
+}
+
+if ($maint->maintnotes != '' AND $mode == 'internal')
+{
+    echo "<tr><th>{$strNotes}:</th><td>{$maint->maintnotes}</td></tr>";
+}
+echo "</table>";
+
+$operations = array();
+$operations[$strEditContract] = array('url' => "contract_edit.php?action=edit&amp;maintid={$id}", 'perm' => PERM_CONTRACT_EDIT);
+
+if ($maint->term != 'yes')
+{
+    $operations[$strNewService] = "contract_new_service.php?contractid={$id}";
+}
+echo "<p align='center'>".html_action_links($operations)."</p>";
+
+echo "<h3>{$strNamedContacts}</h3>";
+
+if (mysql_num_rows($maintresult) > 0)
+{
+    if ($maint->allcontactssupported == 'yes')
+    {
+        echo "<p class='info'>{$strAllSiteContactsSupported}</p>";
+    }
+    else
+    {
+        $allowedcontacts = $maint->supportedcontacts;
+
+        $supportedcontacts = supported_contacts($id);
+        $numberofcontacts = 0;
+
+        $numberofcontacts = sizeof($supportedcontacts);
+        if ($allowedcontacts == 0)
+        {
+            $allowedcontacts = $strUnlimited;
+        }
+        echo "<table class='maintable'>";
+        $supportcount = 1;
+
+        if ($numberofcontacts > 0)
+        {
+            foreach ($supportedcontacts AS $contact)
+            {
+                echo "<tr><th>{$strContact} #{$supportcount}:</th>";
+                echo "<td>".icon('contact', 16)." ";
+                echo "<a href=\"contact_details.php?";
+                echo "id={$contact}\">".contact_realname($contact)."</a>, ";
+                echo contact_site($contact). "</td>";
+
+                echo "<td><a href=\"contract_delete_contact.php?contactid={$contact}&amp;maintid={$id}&amp;context=maintenance\">{$strRemove}</a></td></tr>\n";
+                $supportcount++;
+            }
+        }
+        else
+        {
+            echo "<tr><td>".user_alert($strNoRecords, E_USER_NOTICE)."</td></tr>";
+        }
+        echo "</table>";
+    }
+
+    if ($maint->allcontactssupported != 'yes')
+    {
+        echo "<p align='center'>";
+        echo sprintf($strUsedNofN,
+                        "<strong>".$numberofcontacts."</strong>",
+                        "<strong>".$allowedcontacts."</strong>");
+        echo "</p>";
+
+        if ($numberofcontacts < $allowedcontacts OR $allowedcontacts == 0 AND $mode == 'internal')
+        {
+            echo "<p align='center'><a href='contract_new_contact.php?maintid={$id}&amp;siteid={$maint->site}&amp;context=maintenance'>";
+            echo "{$strNewNamedContact}</a></p>";
+        }
+        else
+        {
+            echo "<h3>{$strNewNamedContact}</h3>";
+            echo "<form action='{$_SERVER['PHP_SELF']}?id={$id}&amp;action=";
+            echo "add' method='post' >";
+            echo "<p align='center'>{$GLOBLAS['strNewSupportedContact']} ";
+            echo contact_site_drop_down('contactid',
+                                            'contactid',
+                                            maintenance_siteid($id),
+                                            supported_contacts($id));
+            echo help_link('NewSupportedContact');
+            echo " <input type='submit' value='{$strNew}' /></p></form>";
+        }
+        if ($mode == 'external')
+        {
+            echo "<p align='center'><a href='newcontact.php'>";
+            echo "{$strNewSiteContact}</a></p>";
+        }
+    }
+
+    echo "<br />";
+    echo "<h3>{$strSkillsSupportedUnderContract}:</h3>";
+    // supported software
+    $sql = "SELECT * FROM `{$GLOBALS[dbSoftwareProducts]}` AS sp, `{$GLOBALS[dbSoftware]}` AS s ";
+    $sql .= "WHERE sp.softwareid = s.id AND productid='{$maint->product}' ";
+    $result = mysql_query($sql);
+    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+
+    if (mysql_num_rows($result)>0)
+    {
+        echo"<table class='maintable'>";
+        while ($software = mysql_fetch_object($result))
+        {
+            $software->lifetime_end = mysql2date($software->lifetime_end);
+            echo "<tr><td> ".icon('skill', 16)." ";
+            if ($software->lifetime_end > 0 AND $software->lifetime_end < $now)
+            {
+                echo "<span class='deleted'>";
+            }
+            echo $software->name;
+            if ($software->lifetime_end > 0 AND $software->lifetime_end < $now)
+            {
+                echo "</span>";
+            }
+            echo "</td></tr>\n";
+        }
+        echo "</table>\n";
+    }
+    else
+    {
+        echo "<p align='center'>{$strNone} / {$strUnknown}<p>";
+    }
+}
+else
+{
+    $html = user_alert($strNothingToDisplay, E_USER_NOTICE);
+}
+
 
 include (APPLICATION_INCPATH . 'htmlfooter.inc.php');
 ?>
--- portal/contracts.php
+++ portal/contracts.php
@@ -61,7 +61,218 @@ include (APPLICATION_INCPATH . 'portalheader.inc.php');
 
 echo "<h2>".icon('contract', 32, $strContract)." {$strContract}</h2>";
 
-echo contract_details($id, 'external');
+$sql  = "SELECT m.*, m.notes AS maintnotes, s.name AS sitename, ";
+$sql .= "r.name AS resellername, lt.name AS licensetypename ";
+$sql .= "FROM `{$dbMaintenance}` AS m, `{$dbSites}` AS s, ";
+$sql .= "`{$dbResellers}` AS r, `{$dbLicenceTypes}` AS lt ";
+$sql .= "WHERE s.id = m.site ";
+$sql .= "AND m.id='{$id}' ";
+$sql .= "AND m.reseller = r.id ";
+$sql .= "AND (m.licence_type IS NULL OR m.licence_type = lt.id) ";
+if ($mode == 'external') $sql .= "AND m.site = '{$_SESSION['siteid']}'";
+
+$maintresult = mysql_query($sql);
+if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+
+$maint = mysql_fetch_object($maintresult);
+
+$html = "<table class='maintable vertical'>";
+echo "<tr><th>{$strContract} {$strID}:</th>";
+echo "<td><h3>".icon('contract', 32)." ";
+echo "{$maint->id}</h3></td></tr>";
+echo "<tr><th>{$strStatus}:</th><td>";
+if ($maint->term == 'yes')
+{
+    echo "<strong>{$strTerminated}</strong>";
+}
+else
+{
+    echo $strActive;
+}
+
+if ($maint->expirydate < $now AND $maint->expirydate != '-1')
+{
+    echo ", <span class='expired'>{$strExpired}</span>";
+}
+echo "</td></tr>\n";
+echo "<tr><th>{$strSite}:</th>";
+
+echo "<td><a href=\"sitedetails.php\">".$maint->sitename."</a></td></tr>";
+
+echo "<tr><th>{$strAdminContact}:</th>";
+
+echo "<td><a href='contactdetails.php?id={$maint->admincontact}'>";
+echo contact_realname($maint->admincontact)."</a></td></tr>";
+
+echo "<tr><th>{$strReseller}:</th><td>";
+
+if (empty($maint->resellername))
+{
+    echo $strNoReseller;
+}
+else
+{
+    echo $maint->resellername;
+}
+echo "</td></tr>";
+echo "<tr><th>{$strProduct}:</th><td>".product_name($maint->product)."</td></tr>";
+echo "<tr><th>{$strIncidents}:</th>";
+echo "<td>";
+$incidents_remaining = $maint->incident_quantity - $maint->incidents_used;
+
+if ($maint->incident_quantity == 0)
+{
+    $quantity = $strUnlimited;
+}
+else
+{
+    $quantity = $maint->incident_quantity;
+}
+
+echo sprintf($strUsedNofN, $maint->incidents_used, $quantity);
+if ($maint->incidents_used >= $maint->incident_quantity AND
+    $maint->incident_quantity != 0)
+{
+    echo " ({$strZeroRemaining})";
+}
+
+echo "</td></tr>";
+if ($maint->licence_quantity != '0')
+{
+    echo "<tr><th>{$strLicense}:</th>";
+    echo "<td>{$maint->licence_quantity} {$maint->licensetypename}</td></tr>\n";
+}
+
+echo "<tr><th>{$strServiceLevel}:</th><td>".get_sla_name($maint->servicelevel)."</td></tr>";
+echo "<tr><th>{$strExpiryDate}:</th><td>";
+if ($maint->expirydate == '-1')
+{
+    echo "{$strUnlimited}";
+}
+else
+{
+    echo ldate($CONFIG['dateformat_date'], $maint->expirydate);
+}
+
+echo "</td></tr>";
+
+if ($maint->maintnotes != '' AND $mode == 'internal')
+{
+    echo "<tr><th>{$strNotes}:</th><td>{$maint->maintnotes}</td></tr>";
+}
+echo "</table>";
+
+echo "<h3>{$strNamedContacts}</h3>";
+
+if (mysql_num_rows($maintresult) > 0)
+{
+    if ($maint->allcontactssupported == 'yes')
+    {
+        echo "<p class='info'>{$strAllSiteContactsSupported}</p>";
+    }
+    else
+    {
+        $allowedcontacts = $maint->supportedcontacts;
+
+        $supportedcontacts = supported_contacts($id);
+        $numberofcontacts = 0;
+
+        $numberofcontacts = sizeof($supportedcontacts);
+        if ($allowedcontacts == 0)
+        {
+            $allowedcontacts = $strUnlimited;
+        }
+        echo "<table class='maintable'>";
+        $supportcount = 1;
+
+        if ($numberofcontacts > 0)
+        {
+            foreach ($supportedcontacts AS $contact)
+            {
+                echo "<tr><th>{$strContact} #{$supportcount}:</th>";
+                echo "<td>".icon('contact', 16)." ";
+                echo "<a href=\"contactdetails.php?";
+                echo "id={$contact}\">".contact_realname($contact)."</a>, ";
+                echo contact_site($contact). "</td>";
+                echo "<td><a href=\"{$_SERVER['PHP_SELF']}?id={$id}&amp;contactid={$contact}&amp;action=remove\">{$strRemove}</a></td></tr>\n";
+                $supportcount++;
+            }
+        }
+        else
+        {
+            echo "<tr><td>".user_alert($strNoRecords, E_USER_NOTICE)."</td></tr>";
+        }
+        echo "</table>";
+    }
+
+    if ($maint->allcontactssupported != 'yes')
+    {
+        echo "<p align='center'>";
+        echo sprintf($strUsedNofN,
+                        "<strong>".$numberofcontacts."</strong>",
+                        "<strong>".$allowedcontacts."</strong>");
+        echo "</p>";
+
+        if ($numberofcontacts < $allowedcontacts OR $allowedcontacts == 0 AND $mode == 'internal')
+        {
+            echo "<p align='center'><a href='contract_new_contact.php?maintid={$id}&amp;siteid={$maint->site}&amp;context=maintenance'>";
+            echo "{$strNewNamedContact}</a></p>";
+        }
+        else
+        {
+            echo "<h3>{$strNewNamedContact}</h3>";
+            echo "<form action='{$_SERVER['PHP_SELF']}?id={$id}&amp;action=";
+            echo "add' method='post' >";
+            echo "<p align='center'>{$GLOBLAS['strNewSupportedContact']} ";
+            echo contact_site_drop_down('contactid',
+                                            'contactid',
+                                            maintenance_siteid($id),
+                                            supported_contacts($id));
+            echo help_link('NewSupportedContact');
+            echo " <input type='submit' value='{$strNew}' /></p></form>";
+        }
+        echo "<p align='center'><a href='newcontact.php'>";
+        echo "{$strNewSiteContact}</a></p>";
+    }
+
+    echo "<br />";
+    echo "<h3>{$strSkillsSupportedUnderContract}:</h3>";
+    // supported software
+    $sql = "SELECT * FROM `{$GLOBALS[dbSoftwareProducts]}` AS sp, `{$GLOBALS[dbSoftware]}` AS s ";
+    $sql .= "WHERE sp.softwareid = s.id AND productid='{$maint->product}' ";
+    $result = mysql_query($sql);
+    if (mysql_error()) trigger_error("MySQL Query Error ".mysql_error(), E_USER_WARNING);
+
+    if (mysql_num_rows($result)>0)
+    {
+        echo"<table class='maintable'>";
+        while ($software = mysql_fetch_object($result))
+        {
+            $software->lifetime_end = mysql2date($software->lifetime_end);
+            echo "<tr><td> ".icon('skill', 16)." ";
+            if ($software->lifetime_end > 0 AND $software->lifetime_end < $now)
+            {
+                echo "<span class='deleted'>";
+            }
+            echo $software->name;
+            if ($software->lifetime_end > 0 AND $software->lifetime_end < $now)
+            {
+                echo "</span>";
+            }
+            echo "</td></tr>\n";
+        }
+        echo "</table>\n";
+    }
+    else
+    {
+        echo "<p align='center'>{$strNone} / {$strUnknown}<p>";
+    }
+}
+else
+{
+    $html = user_alert($strNothingToDisplay, E_USER_NOTICE);
+}
+
 
 include (APPLICATION_INCPATH . 'htmlfooter.inc.php');
 
