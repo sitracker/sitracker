b0c3b4a7725abfe76dc9fa3f29a3c87913bdd48c
Paul Heaney <paul@sitracker.org>
2013-02-24 19:33:38 +0000

    formatting and don't include triggers twice on closure

--- portal/feedback.php
+++ portal/feedback.php
@@ -10,7 +10,7 @@
 //
 // Author Carsten Jensen <carsten[at]sitracker.org>
 
-require ('..'.DIRECTORY_SEPARATOR.'core.php');
+require ('..' . DIRECTORY_SEPARATOR . 'core.php');
 require (APPLICATION_LIBPATH . 'functions.inc.php');
 $accesslevel = 'any';
 require (APPLICATION_LIBPATH . 'portalauth.inc.php');
