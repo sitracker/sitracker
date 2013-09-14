9f9b24fa5ffccc7474ab7753f1b47ea36809941f
Paul Heaney <paul@sitracker.org>
2013-09-14 17:06:46 +0100

    bump revision to beta2

--- lib/base.inc.php
+++ lib/base.inc.php
@@ -23,7 +23,7 @@ if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME']))
 $application_version = '3.90';
 
 // Revision string, e.g. 'beta2' or 'git' or ''
-$application_revision = 'beta1';
+$application_revision = 'beta2';
 
 // The number of errors that have occurred
 $siterrors = 0;
