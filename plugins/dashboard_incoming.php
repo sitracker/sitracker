1111d6b1ce1b1214489f9076dc5715cd05c00f9b
Ivan Lucas <ivan@sitracker.org>
2014-11-20 16:37:22 +0000

    Some improvements for the incoming dashlet to make it work more seamlessly with the v3.90 inbox.  Icon/naming/links

--- plugins/dashboard_incoming.php
+++ plugins/dashboard_incoming.php
@@ -10,6 +10,7 @@
 
 // Author: Martin Cosgrave - apto Solutions Ltd - 20080512
 // updated: Matt Feider - 20090603
+// Updated for SiT 3.90 Inbox - Ivan Lucas - 20141120
 //
 // hacked from dashboard_tasks, updated to match format of dashboard_watch_incidents.php
 
@@ -19,15 +20,15 @@ function dashboard_incoming($dashletid)
 {
     global $sit, $CONFIG, $iconset;
     global $dbUpdates, $dbTempIncoming;
-	
-	$content = "<p align='center'><img src='{$CONFIG['application_webpath']}images/ajax-loader.gif' alt='Loading icon' /></p>";
-	echo dashlet('incoming', $dashletid, icon('emailin', 16), $GLOBALS['strHoldingQueue'], 'holding_queue.php', $content);
+    
+    $content = "<p align='center'><img src='{$CONFIG['application_webpath']}images/ajax-loader.gif' alt='Loading icon' /></p>";
+    echo dashlet('incoming', $dashletid, icon('email', 16), $GLOBALS['strInbox'], 'inbox.php', $content);
 }
 
 function dashboard_incoming_display($dashletid)
 {
-	global $sit, $CONFIG, $iconset;
-	global $dbUpdates, $dbTempIncoming;
+    global $sit, $CONFIG, $iconset;
+    global $dbUpdates, $dbTempIncoming;
     // extract updates (query copied from review_incoming_email.php)
     $sql  = "SELECT u.id AS id, u.bodytext AS bodytext, ti.emailfrom AS emailfrom, ti.subject AS subject, ";
     $sql .= "u.timestamp AS timestamp, ti.incidentid AS incidentid, ti.id AS tempid, ti.locked AS locked, ";
@@ -59,10 +60,10 @@ function dashboard_incoming_display($dashletid)
                 echo "<tr class='$shade'>";
     #            echo "<td><a href='holding_queue.php' class='info'>".truncate_string($incoming->emailfrom, 15);
     #            echo "</a></td>";
-                echo "<td><a href='holding_queue.php' class='info'>".truncate_string($incoming->subject, 25);
+                echo "<td><a href='inbox.php?id=" . $incoming->id . "' class='info'>".truncate_string($incoming->subject, 30);
                 echo "</a></td>";
-                echo "<td><a href='holding_queue.php' class='info'>".truncate_string($incoming->reason, 25);
-                echo "</a></td>";
+                echo "<td>".truncate_string($incoming->reason, 25);
+                echo "/td>";
                 echo "</tr>\n";
                 if ($shade == 'shade1') $shade = 'shade2';
                 else $shade = 'shade1';
@@ -92,6 +93,6 @@ function dashboard_incoming_get_version()
     global $dashboard_incoming_version;
     return $dashboard_incoming_version;
 }
-	
+    
 
 ?>
