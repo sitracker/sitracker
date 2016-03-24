48544b4e2e32742eac4cf7ac3863947b1db4c0c7
Paul Heaney <paul@sitracker.org>
2016-03-24 20:33:08 +0000

    add license types interface - mantis 1978

--- lib/strings.inc.php
+++ lib/strings.inc.php
@@ -167,10 +167,11 @@ if (!empty($_SESSION) AND $_SESSION['auth'] == TRUE)
                            20 => array('perm' => PERM_CONTRACT_ADD, 'name' => $strNewContract, 'url' => "{$CONFIG['application_webpath']}contract_new.php?action=showform"),
                            39 => array('perm' => PERM_RESELLER_ADD, 'name' => $strResellers, 'url' => "{$CONFIG['application_webpath']}reseller.php"),
                            40 => array('perm' => PERM_RESELLER_ADD, 'name' => $strNewReseller, 'url' => "{$CONFIG['application_webpath']}reseller_new.php"),
-                           50 => array('perm' => PERM_SKILL_ADD, 'name' => $strSiteTypes, 'url' => "{$CONFIG['application_webpath']}site_types.php"),
+                           50 => array('perm' => PERM_SITE_TYPES, 'name' => $strSiteTypes, 'url' => "{$CONFIG['application_webpath']}site_types.php"),
                            60 => array('perm' => PERM_CONTRACT_VIEW, 'name' => $strShowRenewals, 'url' => "{$CONFIG['application_webpath']}search_renewals.php?action=showform"),
                            70 => array('perm' => PERM_CONTRACT_VIEW, 'name' => $strShowExpiredContracts, 'url' => "{$CONFIG['application_webpath']}search_expired.php?action=showform"),
-                           80 => array('perm' => PERM_REPORT_RUN, 'name' => $strBilling, 'url' => "{$CONFIG['application_webpath']}billable_incidents.php")
+                           80 => array('perm' => PERM_REPORT_RUN, 'name' => $strBilling, 'url' => "{$CONFIG['application_webpath']}billable_incidents.php"),
+                           90 => array('perm' => PERM_SITE_TYPES, 'name' => $strLicenseTypes, 'url' => "{$CONFIG['application_webpath']}licence_types.php")
     );
     // Second Level: Customers/Feedback  submenu
     if (!is_array(@$hmenu[2050])) $hmenu[2050] = array();
